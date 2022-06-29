13bdf77a2fe44e956dd4c3f0f9cf6cd84c9546d2
Matheus Toledo <matheus.tererj@hotmail.com>
2022-06-28 22:45:17 -0300

    alterações leves

--- src/main/java/com/barbeariatoledo/BarbeariaAPI/Config/SwaggerConfig.java
+++ src/main/java/com/barbeariatoledo/BarbeariaAPI/Config/SwaggerConfig.java
@@ -2,9 +2,11 @@ package com.barbeariatoledo.BarbeariaAPI.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
